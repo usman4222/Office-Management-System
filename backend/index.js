7d5076caa471ae6659f2ef6088fa93ed350ef59e
Rana Usman <usman4243ch@gmail.com>
2023-12-15 23:40:47 -0800

    Connect Mongo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
-import { PORT } from './config.js';
-
+import { PORT, MONGO } from './config.js';
+import mongoose from 'mongoose';
 const app = express();
+
+
+mongoose
+    .connect(MONGO)
+    .then(() => {
+        console.log("Mongo is connected...");
+    })
+    .catch((error) => {
+        console.log("Error while connecting MongoDB", error);
+    });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`);

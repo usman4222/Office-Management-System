86ac3dbd67060a5bc98468a85ecd54c3291c7ccb
Rana Usman <usman4243ch@gmail.com>
2023-12-19 02:42:13 -0800

    Get All User Route

--- backend/controllers/addUserController.js
+++ backend/controllers/addUserController.js
@@ -2,6 +2,8 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 const newUser = require("../models/newUserModel")
 
 
+
+//Add New Employee
 exports.addUser = catchAsyncError(async (req, res, next) => {
     try {
         const { name, fatherName, phone, address, role, designation, userType } = req.body;
@@ -23,3 +25,26 @@ exports.addUser = catchAsyncError(async (req, res, next) => {
         res.status(400).json({ success: false, error: error.message,  });
     }
 })
+
+
+//Get All Employee
+
+
+exports.getAllUsers = catchAsyncError(async (req, res, next) => {
+    try {
+        const users = await newUser.find();
+
+        if (users.length === 0) {
+            return next(new ErrorHandler("No User Found", 400));
+        }
+
+        res.status(200).json({
+            success: true,
+            users,
+            error: { message: "This is an error while getting all users" } 
+        });
+    } catch (error) {
+        return next(new ErrorHandler("Error getting users", 500)); 
+    }
+});
+
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
-const { addUser } = require('../controllers/addUserController');
+const { addUser, getAllUsers } = require('../controllers/addUserController');
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/newuser').post(addUser)
+router.route('/allusers').get(getAllUsers)
 
 module.exports = router;

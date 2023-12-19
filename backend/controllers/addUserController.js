1883169119c7ef3aaab4009f4f5fb8f3082da4ca
Rana Usman <usman4243ch@gmail.com>
2023-12-19 04:00:26 -0800

    Delete User Route

--- backend/controllers/addUserController.js
+++ backend/controllers/addUserController.js
@@ -1,5 +1,6 @@
 const catchAsyncError = require("../middleware/catchAsyncError");
-const newUser = require("../models/newUserModel")
+const newUser = require("../models/newUserModel");
+const ErrorHandler = require("../utils/errorHanlder");
 
 
 
@@ -47,3 +48,22 @@ exports.getAllUsers = catchAsyncError(async (req, res, next) => {
     }
 });
 
+//delete employee
+//Delete user by admin
+exports.deleteEmployee = catchAsyncError(async (req, res, next) => {
+    const user = await newUser.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+        return next(new ErrorHandler(`No User exists with this ID: ${req.params.id}`, 404));
+    }
+
+    // If you're managing images with cloud services like Cloudinary, 
+    // uncomment the code below to delete associated images.
+    // const imageId = user.avatar.public_id
+    // await cloudinary.v2.uploader.destroy(imageId)
+
+    res.status(200).json({
+        success: true,
+        message: "User Deleted successfully"
+    });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
-const { addUser, getAllUsers } = require('../controllers/addUserController');
+const { addUser, getAllUsers, deleteEmployee } = require('../controllers/addUserController');
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
 router.route('/newuser').post(addUser)
 router.route('/allusers').get(getAllUsers)
+router.route('/delete/:id').delete(deleteEmployee)
 
 module.exports = router;

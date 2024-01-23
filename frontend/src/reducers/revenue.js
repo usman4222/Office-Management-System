ae7af0b6d1edd7672d390cf9cf42af9e7762a9e5
Rana Usman <usman4243ch@gmail.com>
2024-01-22 18:33:50 -0800

    Create Revenue Reducer and constant

---
+++ frontend/src/reducers/revenue.js
@@ -0,0 +1,42 @@
+import { CLEAR_ERRORS } from "../constants/financeConstant";
+import { CREATE_REVENUE_FAIL, CREATE_REVENUE_REQUEST, CREATE_REVENUE_RESET, CREATE_REVENUE_SUCCESS } from "../constants/revenue";
+
+
+
+
+
+
+export const financeReducer = (state = { revenue: [] }, action) => {
+
+    switch (action.type) {
+        case CREATE_REVENUE_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+        case CREATE_REVENUE_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                revenue: action.payload.revenue,
+            }
+        case CREATE_REVENUE_RESET:
+            return {
+                ...state,
+                success: false
+            };
+        case CREATE_REVENUE_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+        default:
+            return state
+    }
+}
